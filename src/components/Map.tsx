2aa2201636084887ca91951a84e1f8e8c0b0adf5
Damien PÉDRON <pedron.damien@outlook.fr>
2024-09-13 09:34:18 +0200

    feature/leaflet: add map

---
+++ src/components/Map.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import "leaflet-defaulticon-compatibility";
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
+import { LatLngExpression } from "leaflet";
+
+interface IMapProps {
+    position: LatLngExpression;
+    zoom: number;
+}
+
+export default function Map({ position, zoom }: IMapProps) {
+    return (
+        <MapContainer
+            center={position}
+            zoom={zoom}
+            scrollWheelZoom={true}
+            className="w-screen h-screen"
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={position}>
+                <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
