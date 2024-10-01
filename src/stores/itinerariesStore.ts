e233793c0fbe554992ead90c28c6ae150349f661
Damien PÉDRON <pedron.damien@outlook.fr>
2024-10-01 21:00:54 +0200

    feature: data persistance in local storage

--- src/stores/itinerariesStore.ts
+++ src/stores/itinerariesStore.ts
@@ -1,18 +1,26 @@
 import { IItinerary } from "@/interfaces/itineraryInterface";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface ItinerariesState {
     itineraries: { [key: string]: IItinerary };
     addItinerary: (key: string, newItinerary: IItinerary) => void;
 }
 
-export const useItinerariesStore = create<ItinerariesState>((set) => ({
-    itineraries: {},
-    addItinerary: (key, newItinerary) =>
-        set((state) => ({
-            itineraries: {
-                ...state.itineraries,
-                [key]: newItinerary,
-            },
-        })),
-}));
+export const useItinerariesStore = create<ItinerariesState>()(
+    persist(
+        (set) => ({
+            itineraries: {},
+            addItinerary: (key, newItinerary) =>
+                set((state) => ({
+                    itineraries: {
+                        ...state.itineraries,
+                        [key]: newItinerary,
+                    },
+                })),
+        }),
+        {
+            name: "itineraries-storage",
+        }
+    )
+);
--- src/stores/locationsStore.ts
+++ src/stores/locationsStore.ts
@@ -1,5 +1,6 @@
 import { ILocation } from "@/interfaces/locationInterfaces";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface LocationsState {
     locations: ILocation[];
@@ -11,57 +12,73 @@ interface LocationsState {
     ) => void;
 }
 
-export const useLocationsStore = create<LocationsState>((set) => ({
-    locations: [],
-    addLocation: (newLocation) =>
-        set((state) => ({
-            locations: [...state.locations, newLocation],
-        })),
-    removeLocation: (locationPosition) =>
-        set((state) => {
-            const updatedLocations = state.locations.filter(
-                (location) => location.positionOnMap !== locationPosition
-            );
+export const useLocationsStore = create<LocationsState>()(
+    persist(
+        (set) => ({
+            locations: [],
+            addLocation: (newLocation) =>
+                set((state) => ({
+                    locations: [...state.locations, newLocation],
+                })),
+            removeLocation: (locationPosition) =>
+                set((state) => {
+                    const updatedLocations = state.locations.filter(
+                        (location) =>
+                            location.positionOnMap !== locationPosition
+                    );
 
-            const resortedPositions = updatedLocations.map(
-                (location, index) => ({
-                    ...location,
-                    positionOnMap: index + 1,
-                })
-            );
+                    const resortedPositions = updatedLocations.map(
+                        (location, index) => ({
+                            ...location,
+                            positionOnMap: index + 1,
+                        })
+                    );
 
-            return { locations: resortedPositions };
+                    return { locations: resortedPositions };
+                }),
+            rearrangeLocation: (locationInitialPosition, locationNewPosition) =>
+                set((state) => {
+                    const { locations } = state;
+
+                    const newLocations = [...locations];
+
+                    const initialIndex = newLocations.findIndex(
+                        (location) =>
+                            location.positionOnMap === locationInitialPosition
+                    );
+
+                    if (initialIndex === -1) return state;
+
+                    const [movedLocation] = newLocations.splice(
+                        initialIndex,
+                        1
+                    );
+
+                    newLocations.forEach((location) => {
+                        const isMoveUp =
+                            location.positionOnMap >= locationNewPosition &&
+                            location.positionOnMap < locationInitialPosition;
+                        const isMoveDown =
+                            location.positionOnMap <= locationNewPosition &&
+                            location.positionOnMap > locationInitialPosition;
+
+                        if (isMoveUp || isMoveDown) {
+                            location.positionOnMap += isMoveUp ? 1 : -1;
+                        }
+                    });
+
+                    movedLocation.positionOnMap = locationNewPosition;
+                    newLocations.splice(
+                        locationNewPosition - 1,
+                        0,
+                        movedLocation
+                    );
+
+                    return { ...state, locations: newLocations };
+                }),
         }),
-    rearrangeLocation: (locationInitialPosition, locationNewPosition) =>
-        set((state) => {
-            const { locations } = state;
-
-            const newLocations = [...locations];
-
-            const initialIndex = newLocations.findIndex(
-                (location) => location.positionOnMap === locationInitialPosition
-            );
-
-            if (initialIndex === -1) return state;
-
-            const [movedLocation] = newLocations.splice(initialIndex, 1);
-
-            newLocations.forEach((location) => {
-                const isMoveUp =
-                    location.positionOnMap >= locationNewPosition &&
-                    location.positionOnMap < locationInitialPosition;
-                const isMoveDown =
-                    location.positionOnMap <= locationNewPosition &&
-                    location.positionOnMap > locationInitialPosition;
-
-                if (isMoveUp || isMoveDown) {
-                    location.positionOnMap += isMoveUp ? 1 : -1;
-                }
-            });
-
-            movedLocation.positionOnMap = locationNewPosition;
-            newLocations.splice(locationNewPosition - 1, 0, movedLocation);
-
-            return { ...state, locations: newLocations };
-        }),
-}));
+        {
+            name: "locations-storage",
+        }
+    )
+);
